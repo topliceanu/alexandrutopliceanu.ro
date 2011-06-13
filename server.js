19c25df835e82bbc954fa355360ee2c05d11a6e0
Alexandru Topliceanu <alexandru@ubuntu.localdomain>
2011-06-13 11:46:14 -0700

    changed the port to 80 so it will work with joyent

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get('/', function(req, res){
 
 // Only listen on $ node app.js
 if (!module.parent) {
-  app.listen(3000);
+  app.listen(80);
   console.log("Express server listening on port %d", app.address().port);
 }
