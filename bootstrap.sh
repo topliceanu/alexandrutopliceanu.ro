8cda8ab52a32c1a028b69757c691f1e48f2c4176
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2014-12-28 01:43:32 +0200

    use a repo to install ruby

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
+# install ruby
+sudo apt-get install -y python-software-properties
+sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-get update
-sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
-
-# install ruby.
-sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
-sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -L https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
-rvm install 2.1.5
-rvm use 2.1.5 --default
-ruby -v
-
-# rubygems should not to install the docs locally.
-echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+sudo apt-get install -y ruby rubygems ruby-switch
 
 # install nodejs.
 sudo add-apt-repository ppa:chris-lea/node.js
