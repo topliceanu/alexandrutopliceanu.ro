bb131cce3ba82ff3390379d9705ec68551e3ddca
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2014-12-30 10:32:49 +0200

    add final and correct provision process for vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-# install ruby
-sudo apt-get install -y python-software-properties
-sudo apt-add-repository ppa:brightbox/ruby-ng
-sudo apt-get update
-sudo apt-get install -y ruby rubygems ruby-switch
-
 # install nodejs.
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install nodejs
 
-# install jekyll.
+# install jekyll
 gem install jekyll
+sudo apt-get install ruby1.9.1-dev
