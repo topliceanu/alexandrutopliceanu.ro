3e11b7aeb5f6d2cda216c0b1518c98c90328a71b
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2017-06-22 06:58:00 +0100

    changes to the bootstrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,9 @@ sudo apt-get install -y git
 # install go.
 wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz
 tar -C /usr/local -xzvf go1.7.4.linux-amd64.tar.gz
-kdir -p $HOME/go/src $HOME/go/bin $HOME/go/pkg
+mkdir -p $HOME/go/src $HOME/go/bin $HOME/go/pkg
 echo "export GOPATH=$HOME/go" >> $HOME/.bashrc
-echo "export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin" >> $HOME/.bashrc
+echo "export PATH=$PATH:$HOME/go/bin:/usr/local/go/bin" >> $HOME/.bashrc
 
 # Get hugo
 go get github.com/spf13/hugo
